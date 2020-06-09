c75b65be7406832872f6f55c02657068edcebee5
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-09 10:23:49 +0900

    update

--- database/migrations/2020_06_08_164234_create_restaurants_table.php
+++ database/migrations/2020_06_08_164234_create_restaurants_table.php
@@ -14,11 +14,28 @@ class CreateRestaurantsTable extends Migration
     public function up()
     {
         Schema::create('restaurants', function (Blueprint $table) {
+            // TODO: マイグレーションが通らない。外部キーが上手くできていない？
             $table->id();
             $table->string('name');
-            $table->foreignId('genre')->references('id')->on('cuisine_genre')->onDelete('cascade');
-            $table->string('tel');
-            // todo: マイグレーションの作成
+            $table->foreignId('cuisine_genre_id')->constrained();
+            $table->string('tel')->unique();
+            $table->string('zipcode');
+            $table->foreignId('pref_id')->constrained();
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('gmap_url')->nullable();
+            $table->string('access')->nullable();
+            $table->string('business_hours');
+            $table->string('regular_holiday');
+            $table->boolean('parking_flag');
+            $table->string('parking_text')->nullable();
+            $table->string('credit_card')->nullable();
+            $table->string('electronic_money')->nullable();
+            $table->string('hp_url')->nullable();
+            $table->string('gnavi_url')->nullable();
+            $table->string('tabelog_url')->nullable();
+            $table->string('demaecan_url')->nullable();
+            $table->string('ubereats_url')->nullable();
             $table->timestamps();
         });
     }
