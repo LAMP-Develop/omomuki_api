3ff2e71041280c1b2ab7fa203a7499ca26f238b0
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-22 09:40:16 +0900

    update

---
+++ app/CuisineGenre.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CuisineGenre extends Model
+{
+    protected $table = 'cuisine_genre';
+    protected $primaryKey = 'id';
+}
--- app/Restaurant.php
+++ app/Restaurant.php
@@ -9,7 +9,6 @@ class Restaurant extends Model
     protected $table = 'restaurants';
     protected $primaryKey = 'id';
 
-
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
 }
