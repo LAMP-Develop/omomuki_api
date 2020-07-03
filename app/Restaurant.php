3f6326038bc8c09f385a5f6f76893d5cfa2e13cd
ShunyaSuzuki <anyushu2017@gmail.com>
2020-07-03 12:53:27 +0900

    update

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -3,13 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Restaurant extends Model
 {
-    use QueryCacheable;
-    public $cacheFor = 3600; // cache time, in seconds
-
     protected $table = 'restaurants';
     protected $primaryKey = 'id';
 
